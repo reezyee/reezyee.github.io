4b9b3b7d5d1a79983d2c43a84e280cbc73786e30
Reza Sapitra <113533664+reezyee@users.noreply.github.com>
2024-07-16 19:49:54 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/reezyee.3d/"
+  base: "/reezyee.github.io/"
 })
