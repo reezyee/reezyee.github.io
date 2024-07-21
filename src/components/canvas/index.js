a569fd7e147d6f17f3b58ebd6461d533147b2c25
Reza Sapitra <113533664+reezyee@users.noreply.github.com>
2024-07-21 17:36:37 +0700

    Delete src directory

--- src/components/canvas/index.js
+++
@@ -1,6 +0,0 @@
-import EarthCanvas from "./Earth";
-import BallCanvas from "./Ball";
-import ComputersCanvas from "./Character";
-import StarsCanvas from "./Stars";
-
-export { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas };
